f8ce02731acd230cf7634248c518f0d96ac820cd
giannamgarcia <157261984+giannamgarcia@users.noreply.github.com>
2024-02-17 17:20:03 -0800

    Add files via upload

---
+++ sketch9.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(980,800);
+  box=createSprite(490,400,100,100)
+}
+
+function draw() 
+
+{
+  background("blue");
+drawSprites()
+if(keyIsdown("RIGHT_ARROW")){
+  background("red")
+}
+}
+
